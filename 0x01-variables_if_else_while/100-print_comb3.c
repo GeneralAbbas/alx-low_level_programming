ae43743621eb4cca61dae7faed4e74306124ac95
GeneralAbbas <abbashamza001@gmail.com>
2022-03-28 13:22:47 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - putchar print all possible smallest combination
+ * of two digits with variable type int
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, p;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (p = 49; p < 58; p++)
+		{
+			if (p > i)
+			{
+				putchar(i);
+				putchar(p);
+
+				if (i !=56 || p != 57)
+				{
+					putchar(',');
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
