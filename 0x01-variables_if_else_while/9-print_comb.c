79014cad8b6d9ee4d90cb9cef27c48d31b10c35e
GeneralAbbas <abbashamza001@gmail.com>
2022-03-28 13:02:22 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - putchar prints all possible single digit combination
+ * in ascending order declaring variable as in
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
